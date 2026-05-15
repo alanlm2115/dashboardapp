75463ea93f4d7d2b8239bfdc1fba4f94e0119e02
alanlm2115 <alan.mejia@uab.edu.bo>
2026-05-14 23:12:05 -0400

    cruds de producto y categoria

--- dashboard-app/app/__init__.py
+++ dashboard-app/app/__init__.py
@@ -6,11 +6,12 @@ from .extensions import appbuilder , db
 def create_app() -> Flask:
     app = Flask(__name__)
     app.config.from_object("config")
-
+    
     db.init_app(app)
     with app.app_context():
-        appbuilder.init_app(app, db.session)
+        from .models import Categoria, Producto
         db.create_all()
-        # Registering the views and APIs
-        ...
+        appbuilder.init_app(app, db.session)
+        from . import views
+        
     return app
--- dashboard-app/app/models.py
+++ dashboard-app/app/models.py
@@ -1,12 +1,42 @@
+import datetime
 from flask_appbuilder import Model
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Boolean, Column, DateTime, Integer, Numeric, String, ForeignKey, Text
 from sqlalchemy.orm import relationship
 
-"""
+class Categoria(Model):
+        __tablename__="categoria"
+        id = Column(Integer, primary_key=True)
+        nombre = Column(String(100), nullable=False)
+        descripcion = Column(Text, nullable=True)
+        imagen = Column(String(255), nullable=True)
+        estado = Column(Boolean, nullable=True)
+        creado_en = Column(DateTime, default=datetime.UTC ,nullable=False)
+        actualizado_en = Column(DateTime, default=datetime.UTC, onupdate= datetime.UTC, nullable=False)
 
-You can use the extra Flask-AppBuilder fields and Mixin's
+          
+        productos = relationship(
+                "Producto",
+            back_populates="categorias"
+         )
+        def __repr__(self):
+            return self.nombre
+    
 
-AuditMixin will add automatic timestamp of created and modified by who
-
-
-"""
+class Producto(Model):
+        __tablename__="producto"
+        id = Column(Integer, primary_key=True)
+        nombre = Column(String(100), nullable=False)
+        descripcion = Column(Text, nullable=True)
+        precio = Column(Numeric(10, 2), nullable=True)
+        categoria_id = Column(Integer, ForeignKey("categoria.id"), nullable=False)
+        imagen = Column(String(255), nullable=True)
+        estado = Column(Boolean, nullable=True)
+        creado_en = Column(DateTime, default=datetime.UTC ,nullable=False)
+        actualizado_en = Column(DateTime, default=datetime.UTC, onupdate= datetime.UTC, nullable=False)
+        
+        categorias = relationship(
+                 "Categoria",
+             back_populates="productos"
+        )
+        def __repr__(self):
+            return self.nombre
